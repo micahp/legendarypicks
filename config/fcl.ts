def0a1bad49349432b109632e3ae5481871a84c4
micahp <micahgp@gmail.com>
2025-02-12 21:22:31 -0600

    add nba games/roster api

--- config/fcl.ts
+++ config/fcl.ts
@@ -8,8 +8,21 @@ console.log('Dapp running on network:', flowNetwork)
 
 config({
   'flow.network': flowNetwork,
-  'accessNode.api': ACCESS_NODE_URLS[flowNetwork],
-  'discovery.wallet': `https://fcl-discovery.onflow.org/${flowNetwork}/authn`,
-  'app.detail.icon': 'https://avatars.githubusercontent.com/u/62387156?v=4',
-  'app.detail.title': 'FCL Next Scaffold'
+  'accessNode.api': process.env.NEXT_PUBLIC_FLOW_NETWORK === "local" 
+    ? "http://localhost:8888" 
+    : process.env.NEXT_PUBLIC_FLOW_NETWORK === "testnet"
+    ? "https://rest-testnet.onflow.org"
+    : "https://rest-mainnet.onflow.org",
+  'discovery.wallet': process.env.NEXT_PUBLIC_FLOW_NETWORK === "local"
+    ? "http://localhost:8701/fcl/authn"
+    : process.env.NEXT_PUBLIC_FLOW_NETWORK === "testnet"
+    ? "https://fcl-discovery.onflow.org/testnet/authn"
+    : "https://fcl-discovery.onflow.org/authn",
+  'app.detail.icon': 'https://placekitten.com/g/200/200',
+  'app.detail.title': 'Legendary Picks',
+  '0xLEGENDARYPICKS': process.env.NEXT_PUBLIC_FLOW_NETWORK === "local"
+    ? "0xf8d6e0586b0a20c7"  // This should match your emulator account address
+    : process.env.NEXT_PUBLIC_FLOW_NETWORK === "testnet"
+    ? "0xYOUR_TESTNET_ADDRESS"
+    : "0xYOUR_MAINNET_ADDRESS"
 }).load({ flowJSON })
