af123a6040142ea55ae50861a9979bb2209e8dbe
micahp <micahgp@gmail.com>
2026-08-03 00:27:37 -0500

    fix(build): a test file inside pages/ compiled as a route and broke next build

--- components/Leagues/NflGameLog.test.tsx
+++ components/Leagues/NflGameLog.test.tsx
@@ -1,6 +1,6 @@
 import { render, screen } from '@testing-library/react'
 
-import { NflGameLog } from './[id]'
+import { NflGameLog } from '../../pages/player/[id]'
 
 
 describe('standalone NFL player game log', () => {
